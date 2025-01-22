cad5e9dc59d6ff980bab41d3dd1c548eb4cee8d7
MercyPalleti <palletimercy1@gmail.com>
2025-01-22 18:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  Arrays.sort(arr);
+	let res=0;
+	for(let i=0;i<arr.length;i++){
+		sum+=(arr.length-(i+1))*arr[i];
+	}
+	return sum;
 }
 
 module.exports=mincost;
