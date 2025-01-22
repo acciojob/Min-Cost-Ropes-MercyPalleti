82517aa333ce2eaf53462b043c0eb7ca43cead46
MercyPalleti <palletimercy1@gmail.com>
2025-01-22 18:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mincost(arr)
 //write your code here
 // return the min cost
   Arrays.sort(arr);
-	let res=0;
+	var res=0;
 	for(let i=0;i<arr.length;i++){
 		sum+=(arr.length-(i+1))*arr[i];
 	}
